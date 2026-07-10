7aa7d43b9a3d9bba10e282c0a0f2234c30a34c27
cedricbuche <buche@enib.fr>
2026-07-10 11:41:40 +0930

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -33,3 +33,12 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 });
+
+fetch("sidebar.html")
+  .then(response => response.text())
+  .then(data => {
+      document.getElementById("sidebar").innerHTML = data;
+  })
+  .catch(error => {
+      console.error("Error loading sidebar:", error);
+  });
